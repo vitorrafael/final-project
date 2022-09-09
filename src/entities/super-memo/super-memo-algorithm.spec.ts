51ba3e69ef1e3825df57c48c2c7444e6f7093d7f
vitorrafael <vrsilveira07@gmail.com>
2022-09-09 20:41:35 -0300

    feat: Create entity for SuperMemo algorithm

---
+++ src/entities/super-memo/super-memo-algorithm.spec.ts
@@ -0,0 +1,94 @@
+import { expect } from "chai";
+import { SuperMemoAlgorithm } from "./super-memo-algorithm";
+
+describe("[Entity] SuperMemoAlgorithm", async function () {
+  const superMemoAlgorithm = new SuperMemoAlgorithm();
+
+  it("Should reset the interval for review if the response quality is less than 3", () => {
+    const responseQuality = 2;
+    const eFactor = 3;
+    const currentInterval = 10;
+
+    const algorithmResponse = superMemoAlgorithm.calculateNextReview(
+      responseQuality,
+      eFactor,
+      currentInterval
+    );
+
+    expect(algorithmResponse.eFactor).to.be.equal(eFactor);
+    expect(algorithmResponse.intervalForNextReview).to.be.equal(1);
+  });
+
+  it("Should set eFactor to 1.3 if the calculated eFactor is less than 1.3", () => {
+    const responseQuality = 3;
+    const eFactor = 1.2;
+    const currentInterval = 3;
+
+    const algorithmResponse = superMemoAlgorithm.calculateNextReview(
+      responseQuality,
+      eFactor,
+      currentInterval
+    );
+
+    expect(algorithmResponse.eFactor).to.be.equal(1.3);
+    expect(algorithmResponse.intervalForNextReview).to.be.equal(
+      currentInterval + 1
+    );
+  });
+
+	it("Should calculate and set the eFactor correctly if it is not less than 1.3", () => {
+    const responseQuality = 5;
+    const eFactor = 2.25;
+    const currentInterval = 3;
+
+    const algorithmResponse = superMemoAlgorithm.calculateNextReview(
+      responseQuality,
+      eFactor,
+      currentInterval
+    );
+
+    expect(algorithmResponse.eFactor).to.be.equal(2.35);
+	})
+
+	it("Should calculate and set the nextIntervalForReview correctly if it is at least the third review", () => {
+    const responseQuality = 5;
+    const eFactor = 2.25;
+    const currentInterval = 3;
+
+    const algorithmResponse = superMemoAlgorithm.calculateNextReview(
+      responseQuality,
+      eFactor,
+      currentInterval
+    );
+
+    expect(algorithmResponse.intervalForNextReview).to.be.equal(7);
+	})
+
+	it("Should return pre-defined interval 6 if it is the second review", () => {
+    const responseQuality = 5;
+    const eFactor = 2.25;
+    const currentInterval = 2;
+
+    const algorithmResponse = superMemoAlgorithm.calculateNextReview(
+      responseQuality,
+      eFactor,
+      currentInterval
+    );
+
+    expect(algorithmResponse.intervalForNextReview).to.be.equal(6);
+	})
+
+	it("Should return pre-defined interval 1 if it is at least the first review", () => {
+    const responseQuality = 5;
+    const eFactor = 2.25;
+    const currentInterval = 1;
+
+    const algorithmResponse = superMemoAlgorithm.calculateNextReview(
+      responseQuality,
+      eFactor,
+      currentInterval
+    );
+
+    expect(algorithmResponse.intervalForNextReview).to.be.equal(1);
+	})
+});
---
+++ src/entities/super-memo/super-memo-algorithm.ts
@@ -0,0 +1,44 @@
+export interface SuperMemoAlgorithmResponse {
+  eFactor: number;
+  intervalForNextReview: number;
+}
+
+export class SuperMemoAlgorithm {
+  public calculateNextReview(
+    responseQuality: number,
+    eFactor: number,
+    currentInterval: number
+  ): SuperMemoAlgorithmResponse {
+    if (responseQuality < 3) {
+      return {
+        eFactor,
+        intervalForNextReview: 1,
+      };
+    }
+
+    const nextInterval = this.getNextInterval(eFactor, currentInterval);
+    const newEFactor = this.calculateEFactor(eFactor, responseQuality);
+
+    return {
+      eFactor: newEFactor < 1.3 ? 1.3 : newEFactor,
+      intervalForNextReview: nextInterval,
+    };
+  }
+
+  private getNextInterval(eFactor: number, currentInterval: number): number {
+    if (currentInterval === 1) return 1;
+    if (currentInterval === 2) return 6;
+
+    return Math.round(currentInterval * eFactor);
+  }
+
+  private calculateEFactor(
+    currentEFactor: number,
+    responseQuality: number
+  ): number {
+    return (
+      currentEFactor +
+      (0.1 - (5 - responseQuality) * (0.08 + (5 - responseQuality) * 0.02))
+    );
+  }
+}
