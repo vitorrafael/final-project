0635b79fa2bbd93510bdda20af1990bbb46829d5
vitorrafael <vrsilveira07@gmail.com>
2022-09-11 14:47:48 -0300

    chore: Adds use case to update card

---
+++ src/use-cases/update-card/update-card.spec.ts
@@ -0,0 +1,101 @@
+import { use, expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+import { InMemoryCardRepository } from "../../adapters/repositories/in-memory-card-repository";
+import { RepositoryCardData } from "../create-card/ports/card-repository";
+import { UpdateCard } from "./update-card";
+
+use(chaiAsPromised);
+
+describe("[Use Case] Update Card", () => {
+  it("should update the card front and back and reset review data if request contains changes", async () => {
+    const cardData: RepositoryCardData = {
+      id: "1",
+      front: "What's the answer of everything?",
+      back: "42",
+      eFactor: 3.0,
+      nextReviewDue: new Date(2022, 10, 18),
+      reviewCount: 3,
+    };
+    const repository = new InMemoryCardRepository([cardData]);
+    const useCase = new UpdateCard(repository);
+
+    const updateCardData = {
+      id: "1",
+      front: "Some random question",
+      back: "What?",
+    };
+
+    const updatedCard = await useCase.update(updateCardData);
+
+    expect(updatedCard.front).to.be.equal("Some random question");
+    expect(updatedCard.back).to.be.equal("What?");
+    expect(updatedCard.eFactor).to.be.equal(2.5);
+    expect(updatedCard.nextReviewDue.toDateString()).to.be.equal(
+      new Date().toDateString()
+    );
+    expect(updatedCard.reviewCount).to.be.equal(0);
+  });
+
+  it("should not update card front if it already exists", async () => {
+    const cardData: RepositoryCardData = {
+      id: "1",
+      front: "What's the answer of everything?",
+      back: "42",
+      eFactor: 3.0,
+      nextReviewDue: new Date(2022, 10, 18),
+      reviewCount: 3,
+    };
+    const anotherCardData: RepositoryCardData = {
+      id: "2",
+      front: "How much is 15 plus 13?",
+      back: "38",
+      eFactor: 2.23,
+      nextReviewDue: new Date(2022, 10, 15),
+      reviewCount: 2,
+    };
+    const repository = new InMemoryCardRepository([cardData, anotherCardData]);
+    const useCase = new UpdateCard(repository);
+
+    const updateCardData = {
+      id: "1",
+      front: "How much is 15 plus 13?",
+    };
+
+    const updatedCard = await useCase.update(updateCardData);
+
+    expect(updatedCard.front).to.be.equal("What's the answer of everything?");
+    expect(updatedCard.eFactor).to.be.equal(3);
+    expect(updatedCard.nextReviewDue.toDateString()).to.be.equal(
+      new Date(2022, 10, 18).toDateString()
+    );
+    expect(updatedCard.reviewCount).to.be.equal(3);
+  });
+
+  it("should not reset review information if no changes are passed", async () => {
+    const cardData: RepositoryCardData = {
+      id: "1",
+      front: "What's the answer of everything?",
+      back: "42",
+      eFactor: 3.0,
+      nextReviewDue: new Date(2022, 10, 18),
+      reviewCount: 3,
+    };
+    const repository = new InMemoryCardRepository([cardData]);
+    const useCase = new UpdateCard(repository);
+
+    const updateCardData = {
+      id: "1",
+    };
+
+    const updatedCard = await useCase.update(updateCardData);
+
+    expect(updatedCard.front).to.be.equal("What's the answer of everything?");
+    expect(updatedCard.back).to.be.equal("42");
+    expect(updatedCard.eFactor).to.be.equal(3.0);
+    expect(updatedCard.nextReviewDue.toDateString()).to.be.equal(
+      new Date(2022, 10, 18).toDateString()
+    );
+    expect(updatedCard.reviewCount).to.be.equal(3);
+  });
+});
---
+++ src/use-cases/update-card/update-card.ts
@@ -0,0 +1,74 @@
+import { Card } from "../../entities/card/card";
+import { CardRepository } from "../create-card/ports/card-repository";
+
+export interface UpdateCardRequest {
+  id: string;
+  front?: string;
+  back?: string;
+}
+
+export class UpdateCard {
+  constructor(private readonly cardRepository: CardRepository) {}
+
+  public async update(updateCardData: UpdateCardRequest) {
+    const originalCard = await this.cardRepository.findCardById(
+      updateCardData.id
+    );
+
+    const updatedCard = Card.create({ ...originalCard, ...updateCardData });
+
+    let hasUpdatedCard = false;
+    if (
+      this.shouldUpdateCardFront(updateCardData) &&
+      !(await this.newCardFrontAlreadyExists(updateCardData))
+    ) {
+      await this.cardRepository.updateCardFront(
+        updateCardData.id,
+        updatedCard.front
+      );
+
+      hasUpdatedCard = true;
+    }
+
+    if (this.shouldUpdateCardBack(updateCardData)) {
+      await this.cardRepository.updateCardBack(
+        updateCardData.id,
+        updatedCard.back
+      );
+
+      hasUpdatedCard = true;
+    }
+
+    if (hasUpdatedCard) {
+      await this.resetReviewInformation(updatedCard);
+    }
+
+    return this.cardRepository.findCardById(updateCardData.id);
+  }
+
+  private shouldUpdateCardFront(updateCardData: UpdateCardRequest): boolean {
+    return Object.keys(updateCardData).includes("front");
+  }
+
+  private shouldUpdateCardBack(updateCardData: UpdateCardRequest): boolean {
+    return Object.keys(updateCardData).includes("back");
+  }
+
+  private async newCardFrontAlreadyExists(
+    updateCardData: UpdateCardRequest
+  ): Promise<boolean> {
+    const potentialCard = await this.cardRepository.findCardByFront(
+      updateCardData.front
+    );
+    return Boolean(potentialCard);
+  }
+
+  private async resetReviewInformation(updatedCardData: Card): Promise<void> {
+    return this.cardRepository.update({
+      ...updatedCardData,
+      reviewCount: 0,
+      nextReviewDue: new Date(),
+      eFactor: 2.5,
+    });
+  }
+}
