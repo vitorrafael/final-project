951b11ee04099d7cc6cb173137073a242a30aa45
vitorrafael <vrsilveira07@gmail.com>
2022-09-11 11:55:17 -0300

    chore: Creates interface for card group repository

---
+++ src/adapters/repositories/in-memory-card-group-repository.ts
@@ -0,0 +1,15 @@
+import { CardGroupWithCards } from "../../entities/card-group/card-group";
+import { CardGroupRepository } from "../../use-cases/start-card-group-review/ports/card-group-repository";
+
+export class InMemoryCardGroupRepository implements CardGroupRepository {
+  private cardGroups: CardGroupWithCards[] = [];
+  constructor(cardGroups: CardGroupWithCards[]) {
+    this.cardGroups = cardGroups;
+  }
+
+  public findCardGroupByTheme(theme: String): Promise<CardGroupWithCards> {
+    return Promise.resolve(
+      this.cardGroups.find((cardGroup) => cardGroup.topic === theme)
+    );
+  }
+}
