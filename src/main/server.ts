7bb1c93a8965599f3fd9aaee08e29a10777aceca
vitorrafael <vrsilveira07@gmail.com>
2022-10-09 13:49:55 -0300

    chore: adjusts server initialziation to handle json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,16 +1,22 @@
 import { SQLiteHelper } from "../adapters/repositories/sqlite/helpers/SQLiteHelper";
-import express, { Router } from "express";
+import express, { json, Router } from "express";
 import { setupCardGroupRoutes } from "./routes/card-group-routes";
 import { setupCardRoutes } from "./routes/card-routes";
 
-SQLiteHelper.connect("../db/srs-monolith.db").then(
-  async function startServer() {
+SQLiteHelper.connect("./db/srs-monolith.db").then(
+  function startServer() {
     const app = express();
+
+    app.use(json())
 
     const router = Router();
     app.use("/api", router);
 
     setupCardGroupRoutes(router);
     setupCardRoutes(router);
+
+    app.listen(8000, () => {
+      console.log(`Server running on port: 8000`)
+    })
   }
 );
