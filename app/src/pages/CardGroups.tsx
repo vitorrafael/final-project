be637d33ef780809da84d90f38e07996b9648d5e
vitorrafael <vrsilveira07@gmail.com>
2023-02-26 19:39:18 -0300

    chore: Adds style on hovering list item

--- app/src/pages/CardGroups.tsx
+++ app/src/pages/CardGroups.tsx
@@ -17,13 +17,20 @@ export function CardGroups() {
     fetchCardGroups().then((cardGroups) => setCardGroups(cardGroups));
   }, []);
 
+  function onCardGroupPressed(cardGroupId: number) {
+    console.log("Card Group selected", cardGroupId);
+  }
+
   return (
     <>
       <h2 className="text-xl text-black text-left p-6">Card Groups</h2>
 
       <ul className="bulletless-list">
-        {cardGroups.map(({ topic, description }) => (
-          <li className="p-6 max-w-autox mx-auto bg-white rounded-xl shadow-lg flex-col items-start">
+        {cardGroups.map(({ id, topic, description }) => (
+          <li
+            key={id}
+            className="p-6 max-w-autox mx-auto bg-white rounded-xl shadow-lg flex-col items-start hover:bg-gray-100 hover:cursor-pointer"
+          >
             <div>
               <h3 className="text-left font-medium text-black">{topic}</h3>
               <p className="text-left text-slate-500">{description}</p>
